61f3f6d5266e9919e199eed4fda26ab4e9159ba3
Pariksheet Nanda <pariksheet.nanda@gmail.com>
2012-12-29 09:17:45 -0500

    Add division slider with limits
    Magic numbers have been made as part of the class constructor.  You
    might protest, "You should not have to use a constructor!  This is
    Traits, where one can use it's MyClass(foo=bar) magic initialization".
    The problem with that conventional approach, such as:
    
        class MicropointAttenuator(HasTraits):
            divisions = 90
            division = Range(1, divisions, value=1)
            ...
    
        mp = MicropointAttenuator(divisions=30)
        mp.configure_traits()
    
    ...is the UI does not initialize correctly.  The range slider displays
    a max value of 90, even though the internal Range logic respects the
    value of 30.

--- micropoint_logscale_conversion.py
+++ micropoint_logscale_conversion.py
@@ -13,21 +13,28 @@ __email__ = "p.nanda@andor.com"
 
 from numpy import linspace, log10
 
-from traits.api import HasTraits, Int, Float, Property
+from traits.api import HasTraits, Range, Float, Property
 from traitsui.api import View, Item
 
 
 class MicropointAttenuator(HasTraits):
-    division = Int(1)
+    def __init__(self, divisions=90, min_magnitude=0,
+                 max_magnitude=3, **traits):
+        HasTraits.__init__(self, **traits)
+        self.add_trait('division',
+                       Range(low=1, high=divisions, value=1))
+        self.divisions = divisions
+        self.min_magnitude = min_magnitude
+        self.max_magnitude = max_magnitude
+    
     # FIXME: `percent_transmission' should not be a Property.  There
     # should be a way for the user to change `percent_transmission'
     # and have `division change' in response, but perhaps this
     # circular dependency resolution is not trivial.
     percent_transmission = Property(Float, depends_on=['division'])
     # magnitude = Property(Float, depends_on=['division'])
-
-    def attenuations(self, type="linear",
-                     divisions=90, min_magnitude=0, max_magnitude=3):
+    
+    def attenuations(self, type="linear"):
         '''Returns array of transmission values for the mirror
         attenuator specifications.
 
@@ -37,7 +44,9 @@ class MicropointAttenuator(HasTraits):
         It would be nice to create a new function,
         "estimated_empirical_attenuation()" which uses sampled data to
         populate additional Trait Properties.'''
-        log_attn = linspace(min_magnitude, max_magnitude, divisions)
+        log_attn = linspace(self.min_magnitude,
+                            self.max_magnitude,
+                            self.divisions)
         if (type == "log"):
             return log_attn
         lin_attn = 10 ** log_attn
@@ -51,12 +60,17 @@ class MicropointAttenuator(HasTraits):
 
     # def _get_magnitude
 
-    # FIXME: Add boundary conditions 1 <= division <= 90
-    # TODO: Make `division' into a slider
     view = View(
         Item('division', label='Division'),
         Item('percent_transmission', label='Transmission %')
         )
 
+# If you want to simulate a manual attenuator, you could initialize
+# something like:
+#
+# mp = MicropointAttenuator(divisions=30)
+#
+# TODO: It would be nice to have a radio button in the UI to toggle
+# between manual and motorized presets.
 mp = MicropointAttenuator()
 mp.configure_traits()
